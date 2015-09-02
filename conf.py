9c3ab04ccce7881d6ad823ccaafb756a51c3b62d
Bruno Skvorc <bruno@skvorc.me>
2015-09-02 22:27:22 +0000

    Changed latex filename, possible fix to build error?

--- conf.py
+++ conf.py
@@ -222,7 +222,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'Diffbot PHP Client Documentation.tex', u'Diffbot PHP Client Documentation Documentation',
+  (master_doc, 'DiffbotPHPClientDocumentation.tex', u'Diffbot PHP Client Documentation Documentation',
    u'Diffbot PHP Client Documentation', 'manual'),
 ]
 
